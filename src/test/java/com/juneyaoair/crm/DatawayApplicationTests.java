6ce7ee80c2448b1253aca0be006e4b2d9e9407b4
ArberChang <904105072@qq.com>
2020-05-19 16:39:26 +0800

    init

---
+++ src/test/java/com/juneyaoair/crm/DatawayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juneyaoair.crm;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatawayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
